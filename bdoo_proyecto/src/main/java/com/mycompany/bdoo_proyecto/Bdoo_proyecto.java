e34170e93e4ee388269eacad14bc522a64a3f0da
Alumne Tarda <alumnet@localhost.localdomain>
2026-02-27 19:17:31 +0100

    first commit

---
+++ bdoo_proyecto/src/main/java/com/mycompany/bdoo_proyecto/Bdoo_proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bdoo_proyecto;
+
+/**
+ *
+ * @author alumnet
+ */
+public class Bdoo_proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
