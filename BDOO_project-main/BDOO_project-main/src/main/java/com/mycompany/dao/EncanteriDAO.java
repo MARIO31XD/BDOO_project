d0edd6557ea5ac40e04c0a30bd3b97b2f8e20e94
Lidia Inglés <lidia.ing336@gmail.com>
2026-03-18 16:51:35 +0100

    Refactor crearEncanteri method to include user input

--- BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/dao/EncanteriDAO.java
+++ BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/dao/EncanteriDAO.java
@@ -6,16 +6,93 @@ import com.mycompany.model.Encanteri;
 import com.mycompany.model.Raresa;
 import com.mycompany.util.BDOOUtil;
 import java.util.List;
+import java.util.Scanner;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
-
 public class EncanteriDAO {
-    
-    public void crearEncanteri(Encanteri enc) {
-        
+
+    public void crearEncanteri() {
+
         EntityManager em = BDOOUtil.getEntityManager();
+
+        boolean continuar = false;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Inrodueix nom:");
+        String nom = s.nextLine();
+        //String nom, String descripcio, Raresa raresa, CostMana cost, String tipus, boolean esInstantani
+        System.out.println("Descripció:");
+        String descr = s.nextLine();
+
+        // raresa
+        do {
+
+            System.out.println("Raresa:");
+            System.out.println("[1] Comuna");
+            System.out.println("[2] Rara");
+            System.out.println("[3] Mítica");
+
+            Raresa raresa = null;
+
+            System.out.println("Selecciona raresa: [1, 2, 3]");
+            String r = s.nextLine();
+
+            switch (r) {
+                case "1":
+                    raresa = Raresa.COMUNA;
+                    continuar = true;
+                    break;
+                case "2":
+                    raresa = Raresa.RARA;
+                    continuar = true;
+                    break;
+                case "3":
+                    raresa = Raresa.MITICA;
+                    continuar = true;
+                    break;
+                default:
+                    System.out.println("Raresa incorrecta, torna a provar.");
+                    break;
+            }
+
+        } while (continuar == false);
+
+        //reinicia continuar per reutilitrzarla
+        continuar = false;
+
+        // cost mana
+        System.out.println("Cost mana:");
+
+        System.out.println("Blanc: (introdueix numero sencer)");
+        int blanc = s.nextInt();
+        s.nextLine();
+
+        System.out.println("Blau:");
+        int blau = s.nextInt();
+        s.nextLine();
+
+        System.out.println("Negre:");
+        int negre = s.nextInt();
+        s.nextLine();
+
+        System.out.println("Vermell:");
+        int vermell = s.nextInt();
+        s.nextLine();
+
+        System.out.println("Verd:");
+        int verd = s.nextInt();
+        s.nextLine();
+
+        System.out.println("Incolor:");
+        int incolor = s.nextInt();
+        s.nextLine();
         
+        CostMana cost = new CostMana(blanc, blau, negre, vermell, verd, incolor);
+        
+        
+
         try {
             //comença la transaccio
             em.getTransaction().begin();
@@ -23,9 +100,9 @@ public class EncanteriDAO {
             em.persist(enc);
             //guardar canvis al fitxer
             em.getTransaction().commit();
-            
+
             System.out.println("Encanteri creat.");
-            
+
         } catch (Exception e) {
             if (em.getTransaction().isActive()) {
                 em.getTransaction().rollback();
@@ -34,7 +111,7 @@ public class EncanteriDAO {
         } finally {
             em.close();
         }
-        
+
     }
-    
+
 }
