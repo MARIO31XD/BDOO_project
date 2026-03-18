46fa5c0574cd53a5e62c4784bb32bffd904357c4
Lidia Inglés <lidia.ing336@gmail.com>
2026-03-18 15:51:09 +0100

    Refactor Criatura class with improved getters and setters

--- BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/model/Criatura.java
+++ BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/model/Criatura.java
@@ -10,35 +10,57 @@ public class Criatura extends Carta {
     private String tipusCriatura; 
     private boolean vola;       
 
+
     public Criatura() {
     }
 
+    // Constructor 
     public Criatura(String nom, String descripcio, String edicio, Raresa raresa, CostMana cost, int forca, int resistencia, String tipusCriatura, boolean vola) {
+        
         super(nom, descripcio, edicio, raresa, cost); 
+        
         this.forca = forca;
         this.resistencia = resistencia;
         this.tipusCriatura = tipusCriatura;
         this.vola = vola;
     }
 
-    // Getters i Setters
-    public int getForca() { return forca; }
-    public void setForca(int forca) { this.forca = forca; }
+    public int getForca() {
+        return forca;
+    }
 
-    public int getResistencia() { return resistencia; }
-    public void setResistencia(int resistencia) { this.resistencia = resistencia; }
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
 
-    public String getTipusCriatura() { return tipusCriatura; }
-    public void setTipusCriatura(String tipusCriatura) { this.tipusCriatura = tipusCriatura; }
+    public int getResistencia() {
+        return resistencia;
+    }
 
-    public boolean isVola() { return vola; }
-    public void setVola(boolean vola) { this.vola = vola; }
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public String getTipusCriatura() {
+        return tipusCriatura;
+    }
+
+    public void setTipusCriatura(String tipusCriatura) {
+        this.tipusCriatura = tipusCriatura;
+    }
+
+    public boolean isVola() {
+        return vola;
+    }
+
+    public void setVola(boolean vola) {
+        this.vola = vola;
+    }
 
     public void mostrarInfo() {
         System.out.println("Nom: " + getNom());
         System.out.println("Edició: " + getEdicio());
-        // Aquest mètode requereix que Carta tingui getRaresa()
-        System.out.println("Raresa: " + getRaresa()); 
+        System.out.println("Raresa: " + getRaresa());
         System.out.println("Tipus: " + tipusCriatura);
         System.out.println("P/T: " + forca + "/" + resistencia);
         System.out.println("Vola: " + (vola ? "Sí" : "No"));
