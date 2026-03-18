c24d2addead5d815782d2fd6351f4fcf864bd8cf
Lidia Inglés <lidia.ing336@gmail.com>
2026-03-18 15:51:30 +0100

    Refactor Encanteri class with new attributes and methods
    Updated Encanteri class to include new properties and methods.

--- BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/model/Encanteri.java
+++ BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/model/Encanteri.java
@@ -1,21 +1,65 @@
 package com.mycompany.model;
 
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+
 
 @Entity
+@PrimaryKeyJoinColumn(name = "encanteri_id")
 public class Encanteri extends Carta {
-    private String tipus;
-    private boolean esPermanent;
+    
+    String tipus; //aura, malediccio, global
+    boolean esInstantani;
+    CostMana cost;
+    
 
-    public Encanteri() {}
-
-    public Encanteri(String nom, String descripcio, String edicio, Raresa raresa, CostMana cost, String tipus, boolean esPermanent) {
+    public Encanteri(String nom, String descripcio, String edicio, Raresa raresa, CostMana cost, String tipus, boolean esInstantani) {
         super(nom, descripcio, edicio, raresa, cost);
         this.tipus = tipus;
-        this.esPermanent = esPermanent;
+        this.esInstantani = esInstantani;
+        this.raresa = raresa;
+        this.cost = cost;
     }
 
-    // Getters y Setters
-    public String getTipus() { return tipus; }
-    public boolean isEsPermanent() { return esPermanent; }
+    @Override
+    public String toString() {
+        return "Encanteri: " + " | Tipus: " + tipus + " | Es Instantani?: " + esInstantani;
+    }
+    
+    public String getTipus() {
+        return tipus;
+    }
+
+    public boolean isEsInstantani() {
+        return esInstantani;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setTipus(String tipus) {
+        this.tipus = tipus;
+    }
+
+    public void setEsInstantani(boolean esInstantani) {
+        this.esInstantani = esInstantani;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+
+    public void setEdicio(String edicio) {
+        this.edicio = edicio;
+    }
+    
 }
