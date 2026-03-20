72452f0ff989c26ed707a2a7f28c0e429f973ee3
Lidia Inglés <lidia.ing336@gmail.com>
2026-03-20 19:40:23 +0100

    ajustes

--- BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/model/Jugador.java
+++ BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/model/Jugador.java
@@ -1,26 +1,103 @@
 package com.mycompany.model;
 
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Jugador {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nick;
-    private int nivell;
+    @GeneratedValue
+    Long id;
+
+    String nick;
+    int nivell;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Mazo> mazos;
 
     @ManyToMany
-    private List<Carta> coleccio = new ArrayList<>();
+    List<Carta> coleccio;
 
-    public Jugador() {}
     public Jugador(String nick, int nivell) {
         this.nick = nick;
         this.nivell = nivell;
+        this.mazos = new ArrayList<>();
+        this.coleccio = new ArrayList<>();
     }
 
-    public void addCartaAColeccio(Carta c) { this.coleccio.add(c); }
-    public String getNick() { return nick; }
+    // afegir mazo al jugador
+    public void afegirMazo(Mazo mazo) {
+        this.mazos.add(mazo);
+    }
+
+    // treure mazo del jugador (orphanremoval l'eliminara de la b)
+    public void treureMazo(Mazo mazo) {
+        this.mazos.remove(mazo);
+    }
+
+    // afegir carta a la col·leccio
+    public void afegirCarta(Carta carta) {
+        this.coleccio.add(carta);
+    }
+
+    // treure carta de la col·leccio
+    public void treureCartaColleccio(Carta carta) {
+        this.coleccio.remove(carta);
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador: ID: " + id +
+               " Nick: " + nick +
+               " Nivell: " + nivell +
+               " Mazos: " + mazos.size() +
+               " Cartes a la col·leccio: " + coleccio.size();
+    }
+
+    public Long getId() { 
+        return id; 
+    }
+    
+    public void setId(Long id) {
+        this.id = id; 
+    }
+    
+    public String getNick() {
+        return nick; 
+    }
+    
+    public void setNick(String nick) {
+        this.nick = nick; 
+    }
+    
+    public int getNivell() {
+        return nivell; 
+    }
+    
+    public void setNivell(int nivell) {
+        this.nivell = nivell;
+    }
+    
+    public List<Mazo> getMazos() {
+        return mazos;
+    }
+    
+    public void setMazos(List<Mazo> mazos) {
+        this.mazos = mazos; 
+    }
+    
+    public List<Carta> getColleccio() {
+        return coleccio; 
+    }
+    
+    public void setColleccio(List<Carta> coleccio) {
+        this.coleccio = coleccio; 
+    }
 }
