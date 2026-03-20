e3a481905dfec1831bb54358da7d08094afa29ae
Lidia Inglés <lidia.ing336@gmail.com>
2026-03-20 19:40:39 +0100

    Refactor Mazo class and add Jugador classa

--- BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/model/Mazo.java
+++ BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/model/Mazo.java
@@ -1,88 +1,103 @@
 package com.mycompany.model;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
-public class Mazo {
+public class Jugador {
 
     @Id
     @GeneratedValue
     Long id;
 
-    String nom;
-    LocalDate dataCreacio;
+    String nick;
+    int nivell;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Mazo> mazos;
 
     @ManyToMany
-    List<Carta> cartes;
+    List<Carta> coleccio;
 
-    public Mazo(String nom, LocalDate dataCreacio) {
-        this.nom = nom;
-        this.dataCreacio = dataCreacio;
+    public Jugador(String nick, int nivell) {
+        this.nick = nick;
+        this.nivell = nivell;
+        this.mazos = new ArrayList<>();
+        this.coleccio = new ArrayList<>();
     }
 
-    //afegir una carta
+    // afegir mazo al jugador
+    public void afegirMazo(Mazo mazo) {
+        this.mazos.add(mazo);
+    }
+
+    // treure mazo del jugador (orphanremoval l'eliminara de la b)
+    public void treureMazo(Mazo mazo) {
+        this.mazos.remove(mazo);
+    }
+
+    // afegir carta a la col·leccio
     public void afegirCarta(Carta carta) {
-        this.cartes.add(carta);
+        this.coleccio.add(carta);
+    }
+
+    // treure carta de la col·leccio
+    public void treureCartaColleccio(Carta carta) {
+        this.coleccio.remove(carta);
     }
 
     @Override
     public String toString() {
-        return "Mazo: " + "ID: " + id + " | Nom: " + nom + " | Data de creació: " + dataCreacio + " | Cartes: \n" + imprimirLlistaCartes();
+        return "Jugador: ID: " + id +
+               " Nick: " + nick +
+               " Nivell: " + nivell +
+               " Mazos: " + mazos.size() +
+               " Cartes a la col·leccio: " + coleccio.size();
     }
 
-    public String imprimirLlistaCartes() {
-        //si esta vacio...
-        if (cartes == null || cartes.isEmpty()) {
-            return "El mazo no té cartes.";
-        }
-        //stringbuilder para juntar todos los nombres
-        StringBuilder sb = new StringBuilder();
-        
-        for (Carta carta : cartes) {
-            sb.append("  - ID: ").append(carta.getId())
-                    .append(" | Nom: ").append(carta.getNom())
-                    .append(" | Tipus: ").append(carta.getClass().getSimpleName()) //pillar el tipo de carta que es
-                    .append("\n");
-        }
-        return sb.toString();
+    public Long getId() { 
+        return id; 
     }
-
-    public Long getId() {
-        return id;
-    }
-
+    
     public void setId(Long id) {
-        this.id = id;
+        this.id = id; 
     }
-
-    public String getNom() {
-        return nom;
+    
+    public String getNick() {
+        return nick; 
     }
-
-    public LocalDate getDataCreacio() {
-        return dataCreacio;
+    
+    public void setNick(String nick) {
+        this.nick = nick; 
     }
-
-    public void setNom(String nom) {
-        this.nom = nom;
+    
+    public int getNivell() {
+        return nivell; 
     }
-
-    public void setDataCreacio(LocalDate dataCreacio) {
-        this.dataCreacio = dataCreacio;
+    
+    public void setNivell(int nivell) {
+        this.nivell = nivell;
     }
-
-    public List<Carta> getCartes() {
-        return cartes;
+    
+    public List<Mazo> getMazos() {
+        return mazos;
     }
-
-    public void setCartes(List<Carta> cartes) {
-        this.cartes = cartes;
+    
+    public void setMazos(List<Mazo> mazos) {
+        this.mazos = mazos; 
     }
-
+    
+    public List<Carta> getColleccio() {
+        return coleccio; 
+    }
+    
+    public void setColleccio(List<Carta> coleccio) {
+        this.coleccio = coleccio; 
+    }
 }
