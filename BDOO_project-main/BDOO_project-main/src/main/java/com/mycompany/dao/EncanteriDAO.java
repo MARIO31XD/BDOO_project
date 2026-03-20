9a351e07b6002a6c28d5774ea3360d9fb930bdd8
Lidia Inglés <lidia.ing336@gmail.com>
2026-03-20 17:33:33 +0100

    Refactor mana cost input handling with exception handling
    Refactor input handling for mana cost to use parseInt with exception handling, improving user input validation.

--- BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/dao/EncanteriDAO.java
+++ BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/dao/EncanteriDAO.java
@@ -5,6 +5,7 @@ import com.mycompany.model.CostMana;
 import com.mycompany.model.Encanteri;
 import com.mycompany.model.Raresa;
 import com.mycompany.util.BDOOUtil;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -66,44 +67,49 @@ public class EncanteriDAO {
 
         // cost mana
         System.out.println("Cost mana:");
+        CostMana cost = null;
 
-        System.out.println("Blanc: (introdueix numero sencer)");
-        int blanc = s.nextInt();
-        s.nextLine();
+        do {
+            try {
+                System.out.println("Blanc: (introdueix numero sencer)");
+                int blanc = Integer.parseInt(s.nextLine());
 
-        System.out.println("Blau:");
-        int blau = s.nextInt();
-        s.nextLine();
+                System.out.println("Blau:");
+                int blau = Integer.parseInt(s.nextLine());
 
-        System.out.println("Negre:");
-        int negre = s.nextInt();
-        s.nextLine();
+                System.out.println("Negre:");
+                int negre = Integer.parseInt(s.nextLine());
 
-        System.out.println("Vermell:");
-        int vermell = s.nextInt();
-        s.nextLine();
+                System.out.println("Vermell:");
+                int vermell = Integer.parseInt(s.nextLine());
 
-        System.out.println("Verd:");
-        int verd = s.nextInt();
-        s.nextLine();
+                System.out.println("Verd:");
+                int verd = Integer.parseInt(s.nextLine());
 
-        System.out.println("Incolor:");
-        int incolor = s.nextInt();
-        s.nextLine();
+                System.out.println("Incolor:");
+                int incolor = Integer.parseInt(s.nextLine());
 
-        CostMana cost = new CostMana(blanc, blau, negre, vermell, verd, incolor);
+                cost = new CostMana(blanc, blau, negre, vermell, verd, incolor);
+                continuar = true;
+
+            } catch (NumberFormatException e) {
+                System.out.println("Has introduit un caràcter que no és un numero, torna a intentar.");
+            }
+        } while (!continuar);
 
         // tipus
         System.out.println("Introdueix tipus:");
         String tipus = s.nextLine();
 
+        continuar = false;
+        
         // es instantania        
         do {
 
             System.out.println("Es instantani? [SI / NO]");
             String resposta = s.nextLine();
             // passem a minuscules
-            String r = resposta.toLowerCase();
+            String r = resposta.trim().toLowerCase();
             // comprobem
             if (r.equals("si")) {
                 esInstantani = true;
@@ -126,11 +132,11 @@ public class EncanteriDAO {
 
     //actualitzar encanteri
     public Carta actualitzarEncanteri(Encanteri enc) {
-        
+
         CartaDAO cartaDAO = new CartaDAO();
-        
+
         Scanner s = new Scanner(System.in);
-        
+
         boolean continuar = false;
 
         Raresa raresa = null;
@@ -179,38 +185,43 @@ public class EncanteriDAO {
 
         // cost mana
         System.out.println("Cost mana:");
+        CostMana cost = null;
 
-        System.out.println("Blanc: (introdueix numero sencer)");
-        int blanc = s.nextInt();
-        s.nextLine();
+        do {
+            try {
+                System.out.println("Blanc: (introdueix numero sencer)");
+                int blanc = Integer.parseInt(s.nextLine());
 
-        System.out.println("Blau:");
-        int blau = s.nextInt();
-        s.nextLine();
+                System.out.println("Blau:");
+                int blau = Integer.parseInt(s.nextLine());
 
-        System.out.println("Negre:");
-        int negre = s.nextInt();
-        s.nextLine();
+                System.out.println("Negre:");
+                int negre = Integer.parseInt(s.nextLine());
 
-        System.out.println("Vermell:");
-        int vermell = s.nextInt();
-        s.nextLine();
+                System.out.println("Vermell:");
+                int vermell = Integer.parseInt(s.nextLine());
 
-        System.out.println("Verd:");
-        int verd = s.nextInt();
-        s.nextLine();
+                System.out.println("Verd:");
+                int verd = Integer.parseInt(s.nextLine());
 
-        System.out.println("Incolor:");
-        int incolor = s.nextInt();
-        s.nextLine();
+                System.out.println("Incolor:");
+                int incolor = Integer.parseInt(s.nextLine());
 
-        CostMana cost = new CostMana(blanc, blau, negre, vermell, verd, incolor);
+                cost = new CostMana(blanc, blau, negre, vermell, verd, incolor);
+                continuar = true;
+
+            } catch (NumberFormatException e) {
+                System.out.println("Has introduit un caràcter que no és un numero, torna a intentar.");
+            }
+        } while (!continuar);
 
         // tipus
         System.out.println("Introdueix tipus: [" + enc.getTipus() + " ]");
         String tipus = s.nextLine();
 
-        // es instantania        
+        // es instantania     
+        continuar = false;
+
         do {
 
             System.out.println("Es instantani? [SI / NO] [" + enc.isEsInstantani() + " ]");
@@ -229,7 +240,7 @@ public class EncanteriDAO {
             }
 
         } while (!continuar);
-        
+
         enc.setNom(nom);
         enc.setDescripcio(descr);
         enc.setEdicio(edicio);
@@ -237,12 +248,12 @@ public class EncanteriDAO {
         enc.setCost(cost);
         enc.setTipus(tipus);
         enc.setEsInstantani(esInstantani);
-        
+
         cartaDAO.actualitzarCarta(enc);
-        
+
         return enc;
     }
-    
+
     public Encanteri obtenirEncanteriPerID(int id) {
 
         EntityManager em = BDOOUtil.getEntityManager();
@@ -254,139 +265,4 @@ public class EncanteriDAO {
         return query.getSingleResult();
     }
 
-    
-}
-package com.mycompany.dao;
-
-import com.mycompany.dao.*;
-import com.mycompany.model.CostMana;
-import com.mycompany.model.Encanteri;
-import com.mycompany.model.Raresa;
-import com.mycompany.util.BDOOUtil;
-import java.util.List;
-import java.util.Scanner;
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
-public class EncanteriDAO {
-
-    public void crearEncanteri() {
-
-        CartaDAO cartaDAO = new CartaDAO();
-
-        boolean continuar = false;
-
-        Raresa raresa = null;
-        boolean esInstantani = false;
-
-        Scanner s = new Scanner(System.in);
-
-        //comença la transaccio
-        System.out.println("Inrodueix nom:");
-        String nom = s.nextLine();
-        System.out.println("Descripció:");
-        String descr = s.nextLine();
-        System.out.println("Edició:");
-        String edicio = s.nextLine();
-
-        // raresa
-        do {
-
-            System.out.println("Raresa:");
-            System.out.println("[1] Comuna");
-            System.out.println("[2] Rara");
-            System.out.println("[3] Mítica");
-
-            System.out.println("Selecciona raresa: [1, 2, 3]");
-            String r = s.nextLine();
-
-            switch (r) {
-                case "1":
-                    raresa = Raresa.COMUNA;
-                    continuar = true;
-                    break;
-                case "2":
-                    raresa = Raresa.RARA;
-                    continuar = true;
-                    break;
-                case "3":
-                    raresa = Raresa.MITICA;
-                    continuar = true;
-                    break;
-                default:
-                    System.out.println("Raresa incorrecta, torna a provar.");
-                    break;
-            }
-
-        } while (!continuar);
-
-        //reinicia continuar per reutilitrzarla
-        continuar = false;
-
-        // cost mana
-        System.out.println("Cost mana:");
-
-        System.out.println("Blanc: (introdueix numero sencer)");
-        int blanc = s.nextInt();
-        s.nextLine();
-
-        System.out.println("Blau:");
-        int blau = s.nextInt();
-        s.nextLine();
-
-        System.out.println("Negre:");
-        int negre = s.nextInt();
-        s.nextLine();
-
-        System.out.println("Vermell:");
-        int vermell = s.nextInt();
-        s.nextLine();
-
-        System.out.println("Verd:");
-        int verd = s.nextInt();
-        s.nextLine();
-
-        System.out.println("Incolor:");
-        int incolor = s.nextInt();
-        s.nextLine();
-
-        CostMana cost = new CostMana(blanc, blau, negre, vermell, verd, incolor);
-
-        // tipus
-        System.out.println("Introdueix tipus:");
-        String tipus = s.nextLine();
-
-        // es instantania        
-        do {
-
-            System.out.println("Es instantani? [SI / NO]");
-            String resposta = s.nextLine();
-            // passem a minuscules
-            String r = resposta.toLowerCase();
-            // comprobem
-            if (r.equals("si")) {
-                esInstantani = true;
-                continuar = true;
-            } else if (r.equals("no")) {
-                esInstantani = false;
-                continuar = true;
-            } else {
-                System.out.println("Resposta incorrecta, torna a escriure SI o NO.");
-            }
-
-        } while (!continuar);
-
-        //creem objecte
-        Encanteri enc = new Encanteri(nom, descr, edicio, raresa, cost, tipus, esInstantani);
-        //creem objecte per a la bd
-        cartaDAO.crearCarta(enc);
-
-    }
-
-    //actualitzar encanteri
-    public Encanteri actualitzarEncanteri(Encanteri enc) {
-        return enc;
-    }
-
-    //eliminar encanteri
 }
