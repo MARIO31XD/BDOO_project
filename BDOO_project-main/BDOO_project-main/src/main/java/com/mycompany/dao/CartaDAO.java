eef4cf3a50b119d3695f7432636cd6b3396db757
Lidia Inglés <lidia.ing336@gmail.com>
2026-03-20 14:38:02 +0100

    carta dao funcionando

--- BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/dao/CartaDAO.java
+++ BDOO_project-main/BDOO_project-main/src/main/java/com/mycompany/dao/CartaDAO.java
@@ -1,7 +1,6 @@
 package com.mycompany.dao;
 
 import com.mycompany.model.Carta;
-import com.mycompany.model.Encanteri;
 import com.mycompany.util.BDOOUtil;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -9,7 +8,7 @@ import javax.persistence.TypedQuery;
 
 public class CartaDAO {
 
-    //crear
+    // CREATE
     public void crearCarta(Carta c) {
 
         EntityManager em = BDOOUtil.getEntityManager();
@@ -26,6 +25,7 @@ public class CartaDAO {
             System.out.println("Carta creada.");
 
         } catch (Exception e) {
+            System.out.println("ERROR: No s'ha pogut crear la carta.");
             if (em.getTransaction().isActive()) {
                 em.getTransaction().rollback();
             }
@@ -35,6 +35,30 @@ public class CartaDAO {
         }
     }
 
+    /// UPDATE
+    
+    public void actualitzarCarta(Carta c) {
+
+        EntityManager em = BDOOUtil.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            //merge perque la carta ve de fora. el merge ja sap on guardarla
+            em.merge(c);
+            em.getTransaction().commit();
+            System.out.println("Carta actualitzada.");
+        } catch (Exception e) {
+            System.out.println("ERROR: Carta no actualitzada.");
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    // READ
     //obtenir per id
     public Carta obtenirCartaPerID(int id) {
 
@@ -43,10 +67,16 @@ public class CartaDAO {
         TypedQuery<Carta> query = em.createQuery("SELECT e FROM Carta e WHERE e.id = :id", Carta.class);
 
         query.setParameter("id", id);
-
-        return query.getSingleResult();
+        
+        Carta result = query.getSingleResult();
+        
+        em.close();
+        
+        return result;
+        
         //para imprimir la carta hacer: Carta c = cDAO.obtenirCartaPerID(id); System.out.print(c);
     }
+
 
     //obtenir totes les cartes
     public void obtenirTotesLesCartes() {
@@ -59,6 +89,34 @@ public class CartaDAO {
             System.out.println(c);
         }
         
+        em.close();
+
+    }
+    
+    // DELETE
+    public void eliminarCarta(Carta c) {
+        
+        EntityManager em = BDOOUtil.getEntityManager();
+        
+        try {
+            em.getTransaction().begin();
+            
+            //antes de borrar hay que acoplar la carta al entitymanager creado aqui
+            Carta cartaMerged = em.merge(c);
+            em.remove(cartaMerged);
+            
+            em.getTransaction().commit();
+            System.out.println("Carta eiminada.");
+        } catch (Exception e) {
+            System.out.println("ERROR: carta no trobada.");
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            e.printStackTrace();
+            
+        } finally {
+            em.close();
+        }
     }
 
 }
